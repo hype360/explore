2501a6fab7a5f1cb6c119432e9fbde49c00461f9
Chamrong THOR <chamrong.thor@allweb.com.kh>
2024-12-18 11:40:22 +0700

    Init blueprint sample seo site

---
+++ ui/src/app/backoffice/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page: React.FC = () => {
+    return (
+        <div style={{textAlign: 'center', marginTop: '50px'}}>
+            <h1>Backoffice</h1>
+            <p>This is the About page of your Next.js app built with TypeScript.</p>
+        </div>
+    );
+};
+
+export default Page;
